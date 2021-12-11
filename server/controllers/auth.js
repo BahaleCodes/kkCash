ed91868b9a97b8c9bff188e0096e8b46be58fb51
ThaboMollo <84439102+ThaboMollo@users.noreply.github.com>
2021-12-11 09:44:19 +0200

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,15 +11,18 @@ exports.signup = (req, res) => {
                 error: errorHandler(err)
             });
         }
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        res.cookie('token', token, { expire: new Date() + 9999 });
         user.salt = undefined;
         user.hashed_password = undefined;
         res.json({
+            token,
+            userId: _id,
             user
         });
     })
 }
 exports.signin = (req, res) => {
-    // find the user based on email
     const { email, password } = req.body;
     User.findOne({ email }, (err, user) => {
         if (err || !user) {
@@ -27,25 +30,20 @@ exports.signin = (req, res) => {
                 error: 'User with that email does not exist. Please signup'
             });
         }
-        // if user is found make sure the email and password match
-        // create authenticate method in user model
         if (!user.authenticate(password)) {
             return res.status(401).json({
                 error: 'Email and password dont match'
             });
         }
-        // generate a signed token with user id and secret
         const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-        // persist the token as 't' in cookie with expiry date
         res.cookie('token', token, { expire: new Date() + 9999 });
-        // return response with user and token to frontend client
         const { _id, name, email, role } = user;
-        return res.json({ token, user: { _id, email, name, role } });
+        return res.json({ token, userId: _id, user: { _id, email, name, role } });
     });
 };
-exports.signout = ( req, res ) => {
+exports.signout = (req, res) => {
     res.clearCookie('token');
-    res.json({ message: "Successfully Signed Out"});
+    res.json({ message: "Successfully Signed Out" });
 };
 
 // Middlewares
