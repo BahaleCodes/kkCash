f77289b6d9cbb044cb8413f81f82e7402ee8bee8
ThaboMollo <84439102+ThaboMollo@users.noreply.github.com>
2021-12-20 14:48:29 +0200

    Responsive Header

--- client/src/screens/Home/Home.js
+++ client/src/screens/Home/Home.js
@@ -4,6 +4,8 @@ import Header from './Header/Header';
 import Intro from './Intro/Intro';
 import Question from './Questions';
 import WhyUs from './WhyUs';
+
+import classes from './Home.module.css';
 import jsonData from '../../data/data.json';
 
 const Home = () => {
@@ -13,8 +15,8 @@ const Home = () => {
     }, []);
 
     return (
-        <div style={{ marginTop: "10rem" }}>
-            <div className='text-center'>
+        <div style={{ marginTop: "10rem" }} className='text-center' >
+            <div className={classes.Header} >
                 <Header />
             </div>
             <Intro />
